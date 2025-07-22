fb9373643ba03c4fcf378c6b79021567a76ffecf
ybc <1340489951@qq.com>
2025-07-22 16:46:41 +0800

    完成菜单的动态渲染

---
+++ src/components/navMenu/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-menu>
+        <MenuItem v-for="item in Menulist" :key="item.url" :item="item"> </MenuItem>
+    </el-menu>
+</template>
+
+<script setup lang="ts">
+import { userUserStore } from '@/stores/auth';
+import MenuItem from '@/components/navMenu/MenuItem.vue';
+const userStore = userUserStore()
+// 获取菜单列表
+const Menulist = userStore.user.menu;
+</script>
---
+++ src/components/navMenu/MenuItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-sub-menu v-if="item.children" :index="item.url">
+        <template #title>
+            <el-icon>
+                <component :is="item.icon"></component>
+            </el-icon>
+            <span>{{ item.name }}</span>
+        </template>
+        <MenuItem v-for="C in item.children" :key="C.url" :item="C"></MenuItem>
+    </el-sub-menu>
+    <el-menu-item v-else :index="item.url">
+        <el-icon>
+            <component :is="item.icon"></component>
+        </el-icon>
+        <span>{{ item.name }}</span>
+    </el-menu-item>
+</template>
+
+<script setup lang="ts" name="MenuItem">
+import { defineProps } from "vue"
+import type { PropType } from 'vue'
+import type { MenuItem } from '@/types/user'
+const props = defineProps({
+    item: {
+        type: Object as PropType<MenuItem>,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped lang="less"></style>
